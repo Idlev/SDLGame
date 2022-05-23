d27a1ab73ec7c53286dac7b49185731037cdc321
Tuomas Aaltonen <tuom4s@hotmail.com>
2022-05-24 00:02:38 +0300

    Add include

---
+++ include/game.h
@@ -0,0 +1,68 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+
+#include "player.h"
+#include "apple.h"
+
+class game
+{
+    public:
+        game();
+        ~game();
+
+        void start();
+
+    private:
+
+    //MAIN LOOP
+    bool is_running;
+    int frame_counter;
+
+    //BUTTONS PRESSES
+    bool key_left;
+    bool key_right;
+
+    //SCREEN
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    static const int SCREEN_WIDTH = 256;
+    static const int SCREEN_HEIGHT = 144;
+
+    //FPS
+    static const int FRAMES_PER_SECOND = 30;
+
+    //BACKGROUND
+    SDL_Surface *background_image;
+    SDL_Texture *background_texture;
+    SDL_Rect background_rect;
+
+    //FUNCTIONS
+    SDL_Surface *load_image(const char* filename);
+    void handle_events();
+    void show_background();
+    void regulate_fps(Uint32 start);
+
+    //GAME
+    void game_logic();
+    bool check_collision(SDL_Rect *rect1,SDL_Rect *rect2);
+
+    //PLAYER
+    player *player1;
+    SDL_Surface *player_image;
+    SDL_Texture *player_texture;
+
+    //APPLES
+    std::vector<apple*> apple_vec;
+    SDL_Surface *apple_image;
+    SDL_Texture *apple_texture;
+    void spawn_apple();
+};
+
+#endif // GAME_H
---
+++ include/player.h
@@ -0,0 +1,38 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+
+#include "SDL2/SDL.h"
+
+class player
+{
+    public:
+        player(SDL_Texture *texture);
+        ~player();
+
+        void show_player(SDL_Renderer *renderer);
+        void move_player_right(SDL_Renderer *renderer, int &frame);
+        void move_player_left(SDL_Renderer *renderer, int &frame);
+        SDL_Rect *get_rect();
+
+        float get_x_vel();
+        void set_x_vel(float vel);
+        void set_x_pos(float pos);
+
+    private:
+
+        SDL_Texture *player_texture;
+        SDL_Rect player_rect;
+        SDL_Rect clips[10];
+
+        float x_pos;
+        float y_pos;
+        float x_vel;
+
+        int player_height;
+        int player_width;
+        int face_dir;
+};
+
+#endif // PLAYER_H
