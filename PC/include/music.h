af47fb6caf1b7eeb2c2dc0d77b70638b3e299670
Tuomas Aaltonen <tuom4s@hotmail.com>
2022-05-29 01:47:19 +0300

    add music class

---
+++ PC/include/music.h
@@ -0,0 +1,18 @@
+#ifndef MUSIC_H
+#define MUSIC_H
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_mixer.h"
+
+class music
+{
+    public:
+        music();
+        virtual ~music();
+
+    protected:
+
+    private:
+};
+
+#endif // MUSIC_H
